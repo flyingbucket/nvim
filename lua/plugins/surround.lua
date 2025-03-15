9303b36db0cda2f93d6387799e093f80a3808867
Miller <hidden@guess.com>
2025-03-15 12:22:52 +0800

    add nvim-surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -4,6 +4,17 @@ return {
     event = "VeryLazy",
     config = function()
         require("nvim-surround").setup({
+            keymaps = {
+                insert = "<C-g>i",
+                normal = "<leader>yr",
+                normal_cur = "<leader>yrr",
+                normal_line = "<leader>yR",
+                normal_cur_line = "<leader>yRR",
+                visual = "R",
+                visual_line = "gR",
+                delete = "<leader>dr",
+                change = "<leader>hr",
+            },
             -- Configuration here, or leave empty to use defaults
         })
     end,
