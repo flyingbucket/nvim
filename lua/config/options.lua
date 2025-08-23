12c6a05c3a544c3a5c322e09a24e88d6482007c0
flyingbucket <2311067@mail.nankai.edu.cn>
2025-08-23 18:52:18 +0800

    cover when editing

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,3 +13,10 @@ opt.tabstop = 4
 -- opt.clipboard:append("unnamedplus")
 -- 设置 clipboard
 opt.clipboard = "unnamedplus,unnamed"
+opt.backup = false -- 不保留永久备份文件（xxx~）
+opt.writebackup = false -- 写入时也不做临时备份
+opt.backupcopy = "yes" -- 直接“就地覆盖”旧文件（不走 rename）
+opt.directory = "~/.cache/nvim/swap//" -- 把 swap 放本地
+opt.backupdir = "~/.cache/nvim/backup//" -- 把备份放本地（虽然后面 backup=false）
+opt.undofile = true
+opt.undodir = "~/.cache/nvim/undo//" -- 把持久化 undo 放本地
