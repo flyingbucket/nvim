79461a16879fc7f3075739c9ae713334473ca13d
flyingbucket <2311067@mail.nankai.edu.cn>
2026-03-25 16:17:36 +0800

    regular commit

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -10,8 +10,8 @@ return {
                 cache = "/home/flyingbucket/.cache/leetcode", -- 可选：缓存目录
             },
             cn = {
-                enabled = true, -- 打开国服站点
-                -- translator = true, -- 使用国服翻译服务
+                -- enabled = true, -- 打开国服站点
+                translator = true, -- 使用国服翻译服务
                 translate_problems = true, -- 显示题面中文翻译
             },
         },
