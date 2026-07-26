6f1116176ee69fdad9d475bb8fea9e5465c3fb4c
flyingbucket <menglinli598@gmail.com>
2026-07-26 19:24:47 +0800

    markdown config

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,13 +1,21 @@
 return {
+    -- {
+    --     "iamcco/markdown-preview.nvim",
+    --     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    --     ft = { "markdown" },
+    --     build = function()
+    --         vim.fn["mkdp#util#install"]()
+    --     end,
+    -- },
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function()
-            vim.fn["mkdp#util#install"]()
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
         end,
+        ft = { "markdown" },
     },
-
     {
         {
             "charmbracelet/glow",
