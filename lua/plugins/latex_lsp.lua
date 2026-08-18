ef063dcc11dd4374dac8372547ac43ddb2fbcdf1
flyingbucket <menglinli598@gmail.com>
2026-08-18 17:53:01 +0800

    lsp: add latex lsp config

---
+++ lua/plugins/latex_lsp.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                ltex_ls_plus = {
+                    enabled = false,
+                },
+                ltex = {
+                    enabled = false,
+                },
+            },
+        },
+    },
+}
