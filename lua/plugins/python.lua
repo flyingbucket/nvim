aa8c4390a0bf7801d141411481eccfc2b51f3ade
flyingbucket <menglinli598@gmail.com>
2026-08-18 20:55:23 +0800

    lsp(python): use gloablly installed astral tools ruff and uv, no mason installation

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,16 +1,3 @@
--- return {
---     {
---         "neovim/nvim-lspconfig",
---         opts = {
---             servers = {
---                 pyright = {
---                 },
---             },
---         },
---     },
--- }
-
--- lua/plugins/python.lua
 return {
     {
         "neovim/nvim-lspconfig",
@@ -18,32 +5,35 @@ return {
             servers = {
                 pyright = {
                     enabled = false,
-                    settings = {
-                        python = {
-                            pythonPath = vim.fn.exepath("python"),
+                },
+                -- 声明 ty 并禁用 Mason 自动安装
+                ty = {
+                    mason = false,
+                },
+                -- 手动配置 ruff
+                ruff = {
+                    mason = false, -- 阻止 Mason 自动下载 ruff，直接使用系统/uv 中的 ruff
+                    cmd = { "ruff", "server" },
+                    init_options = {
+                        settings = {},
+                    },
+                    keys = {
+                        {
+                            "<leader>co",
+                            LazyVim.lsp.action["source.organizeImports"],
+                            desc = "Organize Imports",
                         },
                     },
                 },
-                -- 声明 ty，让 LazyVim 知道该服务存在
-                ty = {},
             },
             setup = {
-                -- 拦截 ty 的默认启动逻辑，用 Neovim 0.11+ 原生 API 接管
                 ty = function(_, opts)
-                    -- 使用 Astral 官方推荐的原生配置方式
                     vim.lsp.config("ty", {
                         settings = {
-                            ty = {
-                                -- 在这里填入 ty 特定配置
-                            },
+                            ty = {},
                         },
                     })
-
-                    -- 启用原生 ty
                     vim.lsp.enable("ty")
-
-                    -- 【核心关键】：返回 true 告诉 LazyVim：
-                    -- "我已经手动处理了 ty，请 lspconfig 不要再用旧的方式启动它了！"
                     return true
                 end,
             },
